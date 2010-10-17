25ab217d1ab7c83f92e94fefbef23b778748ae1c
mohanpriyadarshi <mohanpriyadarshi@yahoo.co.in>
2010-10-17 15:18:00 +0530

    addedfilereadfunction

--- input.php
+++ input.php
@@ -10,4 +10,16 @@ return $input;
 }
 $a=keyinput();
 print "super $a \n";
+#reading from a file and display the content 
+function fileread($fpath)
+{
+print "$fpath \n";
+$inf=fopen($fpath,"r");
+while (!feof($inf))
+{
+$line=fgets($inf,256);
+print "$line";
+}}
+$fpath="readme";
+fileread($fpath);
 ?>
